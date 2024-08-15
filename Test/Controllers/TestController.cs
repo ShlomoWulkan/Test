40a0c630b58da232d0790deb65047af1e8891460
Your Name <you@example.com>
2024-08-15 17:50:36 +0300

    Add project files.

---
+++ Test/Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Test.Controllers
+{
+	public class TestController : Controller
+	{
+
+        public async Task<string> GetUsers()
+        {
+            //Define your baseUrl
+            string baseUrl = "https://fakestoreapi.com/users";
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            var data = await content.ReadAsStringAsync();
+                            return data;
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {                
+                return "";
+            }
+        }
+
+        public async Task<IActionResult> Index()
+		{
+            TempData["Data"] = await GetUsers();    
+			return View();
+		}
+	}
+}
---
+++ Test/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿namespace Test.Models
+{
+	public class Users
+	{
+		public int Id { get; set; }
+
+		public string address { get; set; }
+
+		public string email { get; set; }
+
+		public string username { get; set; }
+
+		public string password { get; set; }
+
+		public string name { get; set; }
+
+		public string phone { get; set; }
+
+	}
+}
